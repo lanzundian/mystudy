5a1bd6671a8ffb3838d0750c4ffd2e6fcfeb4d97
lan <lzd@qq.com>
2025-05-27 19:25:25 +0800

    Keil软件初始工程代码提交，添加了.gitignore文件忽略规则，在main.c中添加了strlen和sizeof函数功能测试。

---
+++ test/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include "ARMCM4.h"   
+#include <stdint.h>
+#include <string.h>
+
+
+
+struct __FILE { int handle; /* Add whatever needed */ };
+FILE __stdout;
+FILE __stdin;
+ 
+int fputc(int ch, FILE *f) 
+{
+    return ITM_SendChar(ch);
+}
+
+volatile int32_t ITM_RxBuffer = ITM_RXBUFFER_EMPTY;
+int fgetc(FILE *f) 
+{
+    while(ITM_CheckChar() != 1)
+	      __NOP();
+    return (ITM_ReceiveChar());
+}
+int ferror(FILE *f) 
+{
+  /* Your implementation of ferror */
+  return EOF;
+}
+
+void _ttywrch(int ch) 
+{
+	fputc(ch, &__stdout);
+}
+
+//int __backspace()
+//{
+//	return 0;
+//}
+void _sys_exit(int return_code) 
+{
+  while (1);    /* endless loop */
+}
+	
+
+
+#define  use_strlen_and_sizeof_test		//strlenºÍsizeofº¯Êý²âÊÔ
+
+
+
+int main(void)
+{
+	
+
+#ifdef	strlen_and_sizeof_test	
+	
+	char  str1[] = { '1','0','0','\0','a','\0','0' };
+	
+	char  str2[6] = { '1','0','0','0','0','0' };
+	
+	printf("%d, %d, %d, %d",strlen(str1), sizeof(str1), strlen(str2), sizeof(str2) );
+#endif
+	
+	
+	return 0;
+	
+	
+}
+
+
